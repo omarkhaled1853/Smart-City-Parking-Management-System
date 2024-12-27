b0b1ee79a1d5230c282e545e6d86e0adec76792f
Hossam-Osama <osamahossam899@gmail.com>
2024-12-27 16:11:37 +0200

    admin report

--- ProjectBackend/src/main/java/com/example/Backend/services/ReportService.java
+++ ProjectBackend/src/main/java/com/example/Backend/services/ReportService.java
@@ -1,16 +1,11 @@
 package com.example.Backend.services;
-
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import javax.sql.DataSource;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
-
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.export.JRPdfExporter;
 import net.sf.jasperreports.export.SimpleExporterInput;
@@ -21,7 +16,6 @@ public class ReportService {
 
     @Autowired
     private DataSource dataSource;
-
 
     public String generateParkingLotReport() throws Exception {
 
@@ -67,13 +61,6 @@ public class ReportService {
         return "Parking Spot Report Done";
     }
 
-    /**
-     * Exports a JasperPrint object to a PDF file.
-     *
-     * @param jasperPrint The populated JasperPrint object.
-     * @param outputFilePath The path to save the PDF file.
-     * @throws Exception If any error occurs during the export process.
-     */
     private void exportReportToPdf(JasperPrint jasperPrint, String outputFilePath) throws Exception {
         File outputFile = new File(outputFilePath);
         try (OutputStream outputStream = new FileOutputStream(outputFile)) {
@@ -83,4 +70,26 @@ public class ReportService {
             exporter.exportReport();
         }
     }
+
+    public String TopUsersAndLotsReport() throws Exception {
+        String jrxmlPath = "src/main/resources/reports/TopUsersAndLotsReport.jrxml";
+        String jasperPath = "src/main/resources/reports/Top Users and Parking Lots Report.jasper";
+        String outputFilePath = "src/main/resources/reports/TopUsersAndLotsReport.pdf";
+            // Compile JRXML to Jasper file
+        JasperReport jasperReport = JasperCompileManager.compileReport(jrxmlPath);
+        JasperCompileManager.writeReportToXmlFile(jasperReport, jasperPath);
+
+        try (Connection connection = dataSource.getConnection()) {
+            // Fill the report with data from the database using the connection
+            Map<String, Object> parameters = new HashMap<>();
+            // You can add parameters to the report here if needed (e.g., filtering by ParkingLotID)
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+
+            // Export the report to PDF
+            exportReportToPdf(jasperPrint, outputFilePath);
+        }
+        return "Top Users and Lots Report Generated Successfully";
+    }
+
+    
 }
